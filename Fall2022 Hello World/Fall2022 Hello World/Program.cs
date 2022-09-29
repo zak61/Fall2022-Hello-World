ea58d8b1e61abedfec3d664b985753ecfad48982
zak61 <112977058+zak61@users.noreply.github.com>
2022-09-29 10:09:24 -0400

    Update Program.cs

--- Fall2022 Hello World/Fall2022 Hello World/Program.cs
+++ Fall2022 Hello World/Fall2022 Hello World/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to the worlds hardest quiz);
+                  
+            var moscow
+            var mogdishu
+            var paris 
+            var mexico city
+            var antartica 
+         
+                  
+            {
+              console.writeline("what is the capital of Russia");
+              console.readline();
+                case "moscow":
+                    Console.WriteLine("Correct");
+                    break;
+              console.writeline("what is the capital of Somalia");
+              console.readline();
+                case "mogdishu":
+                    Console.WriteLine("Correct");
+                    break;
+              console.writeline("what is the capital of France");
+              console.readline();
+                case "paris":
+                    Console.WriteLine("correct");
+                    break;
+              console.writeline("what is the capital of mexico");
+              console.readline();
+                case "mexico city":
+                    Console.WriteLine("corret");
+                    break;
+              console.writeline("Where is the largest desert in the world");
+              console.readline();
+                case "antartica":
+                    Console.WriteLine("correct");
+                    break;
+                default:
+                    Console.WriteLine("incorrect");
+                    break;
+            }
